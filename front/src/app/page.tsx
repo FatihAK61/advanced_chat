c7a5a5df53cf221f901bddb59093246f9ca2e237
FatihAK61 <fatihak61@gmail.com>
2025-08-09 13:26:03 +0300

    Update

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Kahpe !Get started by editing{" "}
+            Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Amk Fahişe Zübeydesi! Sikik !
+            Save and see your changes instantly.
           </li>
         </ol>
 
