41266e0b3e3d8b59237cc66ea7d1fb4d12b4346a
FatihAK61 <fatihak61@gmail.com>
2025-08-09 12:34:06 +0300

    Update

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Amk Fahişe Zübeydesi!
+            Amk Fahişe Zübeydesi! Sikik !
           </li>
         </ol>
 
