f6b933796eb2b0637ee33884f69aca2ea446b819
FatihAK61 <fatihak61@gmail.com>
2025-08-09 14:21:13 +0300

    Update

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import SessionProvider from "@/providers/SessionProvider";
+import {Toaster} from "@/components/ui/sonner";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -25,11 +27,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-        {children}
-        </body>
+        <SessionProvider>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+            {children}
+            <Toaster richColors duration={5000}/>
+            </body>
+        </SessionProvider>
         </html>
     );
 }
